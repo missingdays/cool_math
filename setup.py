206bba03b02fe8b0240fcbfa1b70484f741e3640
missingdays <rebovykin@gmail.com>
2017-04-30 09:36:34 +0300

    Version 1.5

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ from setuptools import find_packages
 setup(
     name='cool_math',
     packages=find_packages(exclude=['test', '*.test', '*.test.*']),
-    version='1.4',
+    version='1.5',
     description='Cool math',
-    long_description=long_description,
     author='missingdays',
     author_email='rebovykin@gmail.com',
     url='https://github.com/missingdays/cool_math',
-    download_url='https://github.com/missingdays/cool_math/archive/1.4.tar.gz',
+    download_url='https://github.com/missingdays/cool_math/archive/1.5.tar.gz',
     keywords=['math'],
     scripts=['bin/cool_math'],
     install_requires=['scipy']
