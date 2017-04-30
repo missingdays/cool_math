99d1bd8700645f2de16606fa7db1d395e9bf6c3a
missingdays <rebovykin@gmail.com>
2017-04-30 09:12:41 +0300

    Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='cool_math',
+    packages=find_packages(exclude=['test', '*.test', '*.test.*']),
+    version='1.2',
+    description='Cool math',
+    author='missingdays',
+    author_email='rebovykin@gmail.com',
+    url='https://github.com/missingdays/cool_math',
+    download_url='https://github.com/missingdays/cool_math/archive/1.2.tar.gz',
+    keywords=['math'],
+    scripts=['bin/cool_math'],
+    install_requires=['scipy']
+)
